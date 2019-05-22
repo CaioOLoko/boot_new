846ce948a274dc89e6ba63a0f0a01635ee94c30c
Facchetti <facchetti.MATOS012@gmail.com>
2019-05-22 13:33:21 -0300

    Update cabecalho.php

--- visao/cabecalho.php
+++ visao/cabecalho.php
@@ -1,13 +1,23 @@
 <!--Para incluir o cabeçalho no seu site você precisa realizar a requisição deste arquivo `template.php`-->
-<nav>
+<!--<nav>
   <ul>
     <li><a href="./">Principal</a></li>
     <li><a href="./usuario">Usuario</a></li>
-	<?php if(acessoUsuarioEstaLogado()): ?>
+	<?php //if(acessoUsuarioEstaLogado()): ?>
 		<li>Bem vindo <?=acessoPegarUsuarioLogado()?></li>
 		<li><a href="./login/logout">Sair</a></li>
-	<?php else: ?>
+	<?php //else: ?>
 		<li><a href="./login">Entrar</a></li>
-	<?php endif; ?>
+	<?php// endif; ?>
   </ul>
-</nav>
+</nav>-->
+<img src="logo.png">
+<div id="lista">
+	<ul id="menu">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">A Empresa</a></li>
+		<li><a href="#">Produtos</a></li>
+		<li><a href="#">Serviços</a></li>
+		<li><a href="#">Contato</a></li>
+	</ul>
+</div>
