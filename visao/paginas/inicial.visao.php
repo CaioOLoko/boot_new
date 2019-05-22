2af52316d335ab5e1aa1f0507d51bd97a5b7b2bc
Facchetti <facchetti.MATOS012@gmail.com>
2019-05-22 13:24:01 -0300

    Update inicial.visao.php

--- visao/paginas/inicial.visao.php
+++ visao/paginas/inicial.visao.php
@@ -1,5 +1,70 @@
-<h1>Aplicação Web com o MVCd</h1>
-
-<p>Este é um projeto desenvolvido com propósito educacional e não deve ser usado em projetos reais. A ideia geral do projeto é apresentar as ideias e conceitos de arquitetura, modelos e framework para alunos que ainda não conhecem a orientação a objetos.</p> 
-
-<p>O framework utiliza o padrão arquitetural MVC, logo sua estrutura básica apresenta os três principais objetos da arquitetura, representados pelas pastas <code>modelo<code>, <code>controlador</code> e <code>visao</code>. </p>
+<DOCTYPE html>
+<html>
+	<head>
+		<title>Template</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="CaioCampos.css">
+	</head>
+	<body>
+		<div id="pagina">
+			<img src="logo.png">
+			<div id="lista">
+				<ul id="menu">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">A Empresa</a></li>
+					<li><a href="#">Produtos</a></li>
+					<li><a href="#">Serviços</a></li>
+					<li><a href="#">Contato</a></li>
+				</ul>
+			</div>
+			<img id="banner" src="banner_1.jpg">
+			<div id="contem">
+				<div id="conteudo">
+					<h1>Lorem ipsum dolor</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum rutrum nunc elit a
+					adipiscing fermentum. Phasellus condimentum tincidunt faucibus. Lorem ipsum dolor sit nunc
+					amet, consectetur adipiscing elit. Vestibulum rutrum rutrum nunc adipiscing fermentum.<br>
+					Phasellus condimentum tincidunt faucibus.</p>
+					<div id="caixas">
+						<div class="quadrado">
+							<figure class="icon">
+								<img class="icimg" src="img_caixa1.png">
+							</figure>
+							<p class="quad">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="#" class="dolor">Dolor sit ></a>
+						</div>
+						<div class="quadrado">
+						<figure class="icon">
+								<img class="icimg" src="img_caixa2.png">
+							</figure>
+							<p class="quad">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="#" class="dolor">Dolor sit ></a>
+						</div>
+						<div class="quadrado">
+						<figure class="icon">
+								<img class="icimg" src="img_caixa3.png">
+							</figure>
+							<p class="quad">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="#" class="dolor">Dolor sit ></a>
+						</div>
+					</div>
+				</div>
+				<div id="imagem">
+					<img id="aviao" src="img_caixa_direita.jpg">
+				</div>
+			</div>
+			<div id="final">
+				<div>
+					<img id="direitos" src="direitos_autorais.jpg">
+				</div>
+				<div id="redes">
+					<img id="face" src="facebook.jpg">
+					<img id="twitter" src="twitter.jpg">
+				</div>
+				<div>
+					<img id="rodape" src="logo_rodape.jpg">
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
