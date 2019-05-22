1008f3a5b63b48c5ad1674b174d2072bc17a1e1d
Facchetti <facchetti.MATOS012@gmail.com>
2019-05-22 13:19:05 -0300

    Update template.php

--- visao/template.php
+++ visao/template.php
@@ -6,10 +6,21 @@
         <link rel="stylesheet" href="./publico/css/app.css">
     </head>
     <body class="container">
-
+        <?php 
+            // $_SERVER['PHP_SELF'] retorna o caminho do arquivo em que você está
+            if($_SERVER['PHP_SELF']!='/boot_new/index.php/cliente/cadastro/'){
+                require('cabecalho.php');//muda o nome do arquivo se quiser
+            }else{
+                require('cabecalho_alt.php');//muda o nome do arquivo se quiser
+            }
+        ?>
         <main class="container">
             <?php require $viewFilePath; ?>
         </main>
-
+        <?php
+            if($_SERVER['PHP_SELF']!='/boot_new/index.php/cliente/cadastro/'){
+                require('rodape.php');//muda o nome do arquivo se quiser
+            }
+        ?>
     </body>
 </html>
